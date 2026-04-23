c4ab36f87075dd637895993d7391c6d1d7592b0e
Amberchen724 <38272831+Amberchen724@users.noreply.github.com>
2026-04-22 21:09:58 -0400

    Add run.py launcher script

---
+++ run.py
@@ -0,0 +1,7 @@
+import subprocess
+import sys
+import os
+
+if __name__ == "__main__":
+    app_path = os.path.join("artifacts", "mixwise", "app.py")
+    subprocess.run([sys.executable, "-m", "streamlit", "run", app_path])
